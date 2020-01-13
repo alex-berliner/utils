89e0015d2ab7b6f01ee4a529ac1c31e07d052fdf
Alex Berliner <alexberliner@gmail.com>
2020-01-12 21:46:44 -0500

    Month specific time utils

--- time_util.py
+++ time_util.py
@@ -1,27 +1,71 @@
-import time
+from time import *
+from datetime import *
+
 def epoch_to_ymd_hms(epoch):
     try:
+        epoch=int(epoch)
         if epoch > 9999999999:
             epoch /= 1000
-        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
+        return strftime('%Y-%m-%d %H:%M:%S', localtime(epoch))
     except:
         return ""
 
 def epoch_to_md_hm(epoch):
     try:
+        epoch=int(epoch)
         if epoch > 9999999999:
             epoch /= 1000
-        return time.strftime('%m-%d %H:%M', time.localtime(epoch))
+        return strftime('%m-%d %H:%M', localtime(epoch))
     except:
         return ""
 
 def epoch_to_yyyymmdd(epoch):
     try:
+        epoch=int(epoch)
         if epoch > 9999999999:
             epoch /= 1000
-        return time.strftime('%Y%m%d', time.localtime(epoch))
+        return strftime('%Y%m%d', localtime(epoch))
     except:
+        print("could not convert epoch %s"%(epoch))
         return ""
+
+def epoch_month_floor(epoch):
+    try:
+        epoch=int(epoch)
+        if epoch > 9999999999:
+            epoch /= 1000
+        temp = datetime.fromtimestamp(epoch).replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        return int(temp.strftime('%s'))
+    except:
+        print("could not convert epoch %s"%(epoch))
+        return ""
+
+def epoch_month_ceil(epoch):
+    try:
+        epoch=int(epoch)
+        if epoch > 9999999999:
+            epoch /= 1000
+        temp = datetime.fromtimestamp(epoch).replace(day=1, hour=0, minute=0, second=0, microsecond=0) + timedelta(months=1)
+        return int(temp.strftime('%s'))
+    except:
+        print("could not convert epoch %s"%(epoch))
+        return ""
+
+def datetime_month_delta(e1, e2):
+    try:
+        e1 = int(e1)
+        e2 = int(e2)
+        if e1 > 9999999999:
+            e1 /= 1000
+        if e2 > 9999999999:
+            e2 /= 1000
+        e1 = datetime.fromtimestamp(e1)
+        e2 = datetime.fromtimestamp(e2)
+    except:
+        print("could not convert epoch %s %s"%(e1, e2))
+        return ""
+    return (e2.year - e1.year) * 12 + e2.month - e1.month
+
 
 def ms_to_minutes(time_ms):
     return int(time_ms)/1000/60
